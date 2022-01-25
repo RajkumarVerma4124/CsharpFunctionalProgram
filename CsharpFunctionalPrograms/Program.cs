9a47424f11e692bb3de4a9a47c57567da1730567
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-01-25 12:23:50 +0530

    [Raj] Added:- The welcome message in main program

---
+++ CsharpFunctionalPrograms/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpFunctionalPrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Csharp Functional Programs");
+            Console.ReadLine();
+        }
+    }
+}
